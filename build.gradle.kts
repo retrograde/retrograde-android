0afe3064e9fd0c880f64c2b7ce61e0b8d1b32626
Oleg Godovykh <olegg.lieangel@gmail.com>
2018-05-17 22:58:54 -0700

    Update Kotlin to 1.2.41, enforce jdk artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,14 @@ allprojects {
                     }
                 }
                 "com.google.android.gms" -> useVersion(deps.versions.gms)
-                "org.jetbrains.kotlin" -> useVersion(deps.versions.kotlin)
+                "org.jetbrains.kotlin" -> {
+                    if (requested.name.startsWith("kotlin-stdlib-jre")) {
+                        with(requested) {
+                            useTarget("$group:${name.replace("jre", "jdk")}:$version")
+                        }
+                    }
+                    useVersion(deps.versions.kotlin)
+                }
             }
         }
     }
@@ -115,7 +122,7 @@ configurations {
 }
 
 dependencies {
-    "ktlint"("com.github.shyiko:ktlint:0.20.0")
+    "ktlint"(deps.libs.ktlint)
 }
 
 tasks {
@@ -125,6 +132,8 @@ tasks {
     "lintKotlin"(JavaExec::class) {
         main = "com.github.shyiko.ktlint.Main"
         classpath = configurations["ktlint"]
-        args.addAll(listOf("*/src/**/*.kt"))
+        args?.addAll(listOf("*/src/**/*.kt"))
     }
+
+    findByName("check")?.dependsOn("lintKotlin")
 }
